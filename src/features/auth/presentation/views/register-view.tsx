310b35ceb698eed02df24fabf7e4098a01ddf752
vKail <ajurado5411@uta.edu.ec>
2024-11-14 09:07:17 -0500

    Modified the register form for better placement

--- src/features/auth/presentation/views/register-view.tsx
+++ src/features/auth/presentation/views/register-view.tsx
@@ -6,10 +6,10 @@ export const RegisterView = () => {
     return (
         <div className="flex flex-col h-screen  justify-center items-center bg-wite">
             <div className="flex flex-col justify-center w-full max-w-lg p-2 bg-white shadow-md border border-gray-200 rounded-lg ">
-                <h1 className="text-2xl font-semibold ">Crea tu cuenta</h1>
+                <h1 className="text-2xl font-semibold pl-3">Crea tu cuenta</h1>
                 <AuthForm />
                 <div>
-                <span className="text-slate-900 text-sm">Ya tienes una cuenta?</span>
+                <span className="text-slate-900 text-sm pl-3">Ya tienes una cuenta?</span>
                 <Link
                     href="/login"
                 >
