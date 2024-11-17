6d7aa2e87b7e1db6c0ac7b9afaa922e89b93d2e9
vKail <ajurado5411@uta.edu.ec>
2024-11-16 20:18:28 -0500

    Created components for vehicles, cards and filter

--- src/app/dashboard/cars/view/page.tsx
+++ src/app/dashboard/cars/view/page.tsx
@@ -1,13 +1,21 @@
 'use client'
 import { useCarStore } from "@/features/cars/context/car-store";
+import { CarsFilterView } from "@/features/cars/presentation/views/cars-filter-view";
 import { CarsCardView } from "@/features/cars/presentation/views/cars-view";
 
 
 const VehiclePage = () => {
     return (
-        <div>
-            <h1>Vehicle Page</h1>
-           <CarsCardView />
+        <div className="flex flex-row">
+            <div>
+                <CarsFilterView />
+            </div>
+            <div>
+                <h1>Vehicle Page</h1>
+                <CarsCardView />
+            </div>
+            
+           
         </div>
     );
     }
--- src/features/cars/presentation/components/cars-card-component.tsx
+++ src/features/cars/presentation/components/cars-card-component.tsx
@@ -12,17 +12,17 @@ export const CarCardComponent = ({cars} : CarCardViewProps) => {
                 <div className="flex flex-col ">
                     
                     <Image
-                        className="rounded-xl"
+                        className="rounded-xl m-2"
                         src='/images/vehicle.png'
                         alt={car.model || 'Default Model'}
                         width={210}
                         height={200}
                     />
-                    <h1 className="flex"> <p className="font-medium pr-1">Marca:</p> {car.brand}</h1>
-                    <h1 className="flex"> <p className="font-medium pr-1">Modelo:</p> {car.model}</h1>
-                    <h1 className="flex"> <p className="font-medium pr-1">Año:</p> {car.year}</h1>
-                    <h1 className="flex"> <p className="font-medium pr-1">Costo diario:</p> {car.daily_rate}</h1>
-                    <button className=" font-light text-white border rounded-md bg-new-black hover:bg-new-back-hover ">Alquilar</button>
+                    <h1 className="flex mx-2"> <p className="font-medium pr-1">Marca:</p> {car.brand}</h1>
+                    <h1 className="flex mx-2"> <p className="font-medium pr-1">Modelo:</p> {car.model}</h1>
+                    <h1 className="flex mx-2"> <p className="font-medium pr-1">Año:</p> {car.year}</h1>
+                    <h1 className="flex mx-2"> <p className="font-medium pr-1">Costo diario:</p> {car.daily_rate} $</h1>
+                    <button className=" font-light text-white border rounded-md bg-new-black hover:bg-new-back-hover m-2">Alquilar</button>
                 </div>
             ))}
         </div>
