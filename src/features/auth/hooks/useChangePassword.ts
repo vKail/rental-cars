f77eb6407c6ef68635da9919a93ea638993cd5d0
vKail <ajurado5411@uta.edu.ec>
2024-11-25 15:18:07 -0500

    Push for login

--- src/features/auth/hooks/useChangePassword.ts
+++ src/features/auth/hooks/useChangePassword.ts
@@ -2,10 +2,12 @@ import * as yup from 'yup'
 import { AuthDataSourceImpl } from '../services/DataSource'
 import { IRecoveryEmail, IRecoveryPassword } from '../models/IRecovery'
 import toast from 'react-hot-toast'
+import { useRouter } from 'next/router'
 
 
 
 export const useChangePassword = () => {
+    const router = useRouter()
     const initialValues : Partial<IRecoveryPassword> = {
         password: '',
         password_confirmation: '',
@@ -20,6 +22,7 @@ export const useChangePassword = () => {
         values.reset_password_token = token
         try {
             await AuthDataSourceImpl.getInstance().changePassword(values)
+            router.push('/login')
             toast.success('Contraseña cambiada con éxito')
         } catch (error) {
             toast.error('Error al cambiar la contraseña') 
