6f0cc264e5e590d0d232c66c4993c0a97305d94c
Adrian Jurado <adrianisrael.616@gmail.com>
2024-12-19 17:14:54 -0500

    Changed router for email reservarion

--- src/features/auth/hooks/useVerifyEmail.ts
+++ src/features/auth/hooks/useVerifyEmail.ts
@@ -1,12 +1,15 @@
 import toast from "react-hot-toast"
 import { AuthDataSourceImpl } from "../services/DataSource"
+import { useRouter } from "next/navigation"
 
 export const useVerifyEmail = () => {
+    const router = useRouter()
     
     const onSubmit = async (token: string) => {
         try {
             await AuthDataSourceImpl.getInstance().verifyEmail(token)
             toast.success('Email verificado con éxito')
+            router.push('/auth/login')
         } catch (error) {
             toast.error('Error al verificar el email') 
         }
